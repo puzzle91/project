264b1bb670864978568c6425a254895d364d234e
Eduardo Parkinson de Castro <eduardop@tcd.ie>
2018-05-04 14:13:09 +0100

    commit

---
+++ AirportAtlasTester.py
@@ -0,0 +1,32 @@
+
+from Airport import Airport
+import AirportAtlas 
+
+def main():
+        Tester = AirportAtlas("airportcsv.csv")
+        airportTest = Airport
+        test=AirportAtlas
+
+        Code1=input('what is the first airport?')
+        Code2=input('what is the second airport?')
+        Code3=input('what is the third airport?')
+        Code4=input('What is the the fourth airport')
+
+Airport
+Airport = airportModel.CreateInstance
+    
+
+AirportAtlas = Tester.possible_trips(Code1, Code2, Code3, Code4, Code1)
+print(possible_trips = Tester.possibleTrips(Code1,Code2,Code3,Code4,Code1))
+possible_legs = airportModel.possible_routes_costs(airportModel)
+print(trip)
+print(dist)
+trip = (airportModel.calculate_trip(airportCode1,airportCode2,airportCode3,airportCode4))       #print("------Loading-----")
+possibleTrips = Tester.possibleTrips(Code1,Code2,Code3,Code4,Code1)
+print(possibleTrips)
+dist = Tester.getAirportDistance(Code1,Code2)
+
+
+main()
+
+
